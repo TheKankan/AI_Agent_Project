c779a208b58848495563666ec24ee8d3afdbb82f
TheKankan <quentin.lejuez@gmail.com>
2025-07-15 21:19:50 +0200

    AI can choose functions

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,5 +1,25 @@
 import os
 import subprocess
+from google.genai import types
+
+schema_run_python = types.FunctionDeclaration(
+    name="run_python",
+    description="runs a python script in the specified directory, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the script to run, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.STRING,
+                description="the arguments to be used in the script",
+            ),
+        },
+    ),
+)
+
 
 def run_python_file(working_directory, file_path, args=[]):
     abs_working_directory = os.path.abspath(working_directory)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from google import genai
 import sys
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,14 +14,22 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 user_prompt = sys.argv[1]
+
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=messages
+    model="gemini-2.0-flash-001", 
+    contents=messages,
+    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
 )
-print(response.text)
+
+if response.function_calls:
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+if not response.function_calls:
+    print(response.text)
 
 if len(sys.argv) > 2:
     if sys.argv[2] == "--verbose":
